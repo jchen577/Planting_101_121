bd40d8866d566f0e85603ef8b9f2a1ee949492a7
jahliang03 <jahliang@ucsc.edu>
2024-12-01 21:16:25 -0800

    attempting to fix deploy environment

--- src/Scenes/Load.ts
+++ src/Scenes/Load.ts
@@ -6,7 +6,7 @@ export class LoadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.path = "/project/assets/";
+    this.load.path = "./project/assets/";
     this.load.image("smb_tiles", "tilemap.png");
     this.load.spritesheet("all_tiles", "tilemap.png", {
       frameWidth: 64,
