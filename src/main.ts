d0336aeea73c50848dd38abceb9e9b39b2314391
jahliang03 <jahliang@ucsc.edu>
2024-12-02 13:19:53 -0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-import { GameScene } from "./Scenes/Game.ts";
+import { GameScene } from "./Scenes/Game.js";
 import { LoadScene } from "./Scenes/Load.ts";
 
 // Game configuration
