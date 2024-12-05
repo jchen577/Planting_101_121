90f167feba9cc2ad365423f5d87af5c8f0ec18c5
KennyLi6 <kennyli3219@gmail.com>
2024-12-05 15:20:38 -0800

    service worker gets properly made now

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Phaser from "phaser";
 import { GameScene } from "./Scenes/Game.js";
 import { LoadScene } from "./Scenes/Load.js";
 
+if ('serviceWorker' in navigator) {
+	const baseURL = import.meta.env.BASE_URL;
+
+  	navigator.serviceWorker
+		.register(`${baseURL.endsWith('/') ? baseURL : baseURL + '/'}sw.js`)
+		.then((registration) => {
+		console.log('Service Worker registered with scope:', registration.scope);
+		})
+		.catch((error) => {
+		console.error('Service Worker registration failed:', error);
+		});
+}
+
 // Game configuration
 const config = {
 	parent: "phaser-game",
