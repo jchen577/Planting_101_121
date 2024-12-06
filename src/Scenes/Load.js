7e82275362882b13417e91a5688783464569e03f
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-12-06 15:55:16 -0800

    revert change

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -54,7 +54,7 @@ export class LoadScene extends Phaser.Scene {
 
   preload() {
     // Set asset path and preload assets
-    this.load.path = "/assets/";
+    this.load.path = "./seedy_place_in_outer_space/assets/";
 
     this.load.json("lang_en", "lang_eng.json");
     this.load.json("lang_jp", "lang_jp.json");
