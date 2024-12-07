e97ddd3f6395cc51878f9f5771f03068ee7b8c5a
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-12-06 21:16:23 -0800

    Revert "new lockfile"
    This reverts commit e0782527a60036e9ab68184d7792d66daea1b80d.

--- src/Scenes/YAMLInterpreter.js
+++ src/Scenes/YAMLInterpreter.js
@@ -1,10 +1,7 @@
-import { readTextFile } from "https://deno.land/std/fs/mod.ts";
-import YAML from "npm:js-yaml";
+import YAML from 'js-yaml';
+import GameConfig from './GameSettings.yaml?raw';
 
-// Function to dynamically load and parse a YAML file
-export async function loadGameConfig() {
-  const filePath = "./src/Scenes/GameSettings.yaml"; // Replace with the correct path
-  const yamlContent = await readTextFile(filePath); // Read YAML as text
-  const parsedConfig = YAML.load(yamlContent); // Parse YAML to JavaScript object
-  return parsedConfig;
+export function YAMLtoJSObj() {
+    const parsedContent = YAML.load(GameConfig);
+    return parsedContent;
 }
