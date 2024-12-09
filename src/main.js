95514bf6225cdce7492fb398adcc402c1aead5ad
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-12-08 22:19:17 -0800

    scaling issue fix?

--- src/main.js
+++ src/main.js
@@ -1,28 +1,31 @@
 import Phaser from "phaser";
 import { GameScene } from "./Scenes/Game.js";
 import { LoadScene } from "./Scenes/Load.js";
-import {MainMenu} from "./Scenes/MainMenu.js";
+import { MainMenu } from "./Scenes/MainMenu.js";
 
 // Game configuration
 const config = {
-	parent: "phaser-game",
-	type: Phaser.CANVAS,
-	render: {
-		pixelArt: true,
-	},
-	width: 640,
-	height: 640,
-	zoom: 2,
-	input: {
-		keyboard: true,
-	},
-	physics: {
-		default: "arcade",
-		arcade: {
-			debug: true,
-		},
-	},
-	scene: [LoadScene, MainMenu, GameScene],
+  parent: "phaser-game",
+  type: Phaser.CANVAS,
+  render: {
+    pixelArt: true,
+  },
+  width: 640,
+  height: 640,
+  scale: {
+    mode: Phaser.Scale.FIT, // Set scale mode to FIT [1, 5, 7]
+  },
+  zoom: 2,
+  input: {
+    keyboard: true,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  scene: [LoadScene, MainMenu, GameScene],
 };
 
 // Create the Phaser game instance
@@ -35,4 +38,4 @@ const w = Number(game.config.width);
 const h = Number(game.config.height);
 let cursors = null;
 
-export { game, centerX, centerY, w, h, cursors }; 
+export { game, centerX, centerY, w, h, cursors };
