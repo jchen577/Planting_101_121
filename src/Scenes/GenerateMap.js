65fc06dbe586d85414c4da7b83a25169b101a758
KennyLi6 <kennyli3219@gmail.com>
2024-12-08 17:00:40 -0800

    moving yaml to public dist

--- src/Scenes/GenerateMap.js
+++ src/Scenes/GenerateMap.js
@@ -16,7 +16,7 @@ export function generateMap(scene) {
   let colSize = 50;
 
   // Load configuration from YAML
-  const gameConfig = loadGameSettings("./src/Scenes/GameSettings.yaml");
+  const gameConfig = loadGameSettings("/seedy_place_in_outer_space/assets/GameSettings.yaml");
   if (gameConfig) {
     rowSize = gameConfig.tutorial.grid_size[0];
     colSize = gameConfig.tutorial.grid_size[1];
