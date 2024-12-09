f090fb6d71ce1b7523d236e8e5bed2fe0c4315db
KennyLi6 <108485377+KennyLi6@users.noreply.github.com>
2024-12-08 17:26:44 -0800

    changing js-yaml import from npm to cdn

--- src/Scenes/YAMLInterpreter.js
+++ src/Scenes/YAMLInterpreter.js
@@ -1,4 +1,4 @@
-import YAML from "npm:js-yaml";
+import YAML from 'https://cdn.jsdelivr.net/npm/js-yaml@4/dist/jsyaml.min.js';
 export async function loadGameSettings(filePath) {
   const response = await fetch(filePath);
   if (!response.ok) {
